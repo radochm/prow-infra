fba76f56baec9eaf1f31f78f9d0a68312ef92d0e
chmrad <128817020+chmrad@users.noreply.github.com>
2023-05-08 18:55:54 +0200

    Update gce_run.sh

--- e2e/provision/gce_run.sh
+++ e2e/provision/gce_run.sh
@@ -15,13 +15,13 @@ venv/bin/ansible-galaxy collection install kubernetes.core
 ssh-keygen -b 2048 -t rsa -f /home/ubuntu/.ssh/id_rsa -q -N ""
 cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 
-apt-get -y install gcc-12
+sudo apt-get -y install gcc-12
 git clone -b v0.6.8 https://github.com/free5gc/gtp5g.git
 cd gtp5g
 make
-make install
-modprobe gtp5g
-dmesg | tail -n 4
+sudo make install
+sudo modprobe gtp5g
+sudo dmesg | tail -n 4
 
 
 #git clone https://github.com/nephio-project/one-summit-22-workshop.git
